b509c4065830dc2926e28b45ada111e07f24a229
Veida <hernandez.veida@gmail.com>
2021-03-29 17:23:25 -0700

    No More React

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,14 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Smart Garden Frontend</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+ 
       </header>
     </div>
   );
