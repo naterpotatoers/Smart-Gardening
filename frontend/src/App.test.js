9ae621ee1f1a4e8ed46eb2f53609050f3f944247
Veida <hernandez.veida@gmail.com>
2021-04-12 16:35:25 -0700

    Router added, Create & Home page added, Styling of pages added

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,11 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
+
+
 test("renders learn react link", () => {
   render(<App />);
+  
   const title = screen.getByText("Smart Garden Frontend");
   expect(title).toBeInTheDocument();
 });
