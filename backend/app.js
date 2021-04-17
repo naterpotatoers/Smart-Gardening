2722c2f1e1021334cec869eba1acebb7cb606da5
naterpotatoers <nate.garza@hotmail.com>
2021-04-17 01:11:55 -0700

    removes /views and cleans up endpoints for backend

--- backend/app.js
+++ backend/app.js
@@ -1,17 +1,13 @@
-var createError = require("http-errors");
 var express = require("express");
-var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+var createError = require("http-errors");
+var path = require("path");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 
 var app = express();
-
-// view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "jade");
 
 app.use(logger("dev"));
 app.use(express.json());
@@ -35,7 +31,7 @@ app.use(function (err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render("error");
+  res.send(err.message);
 });
 
 module.exports = app;
