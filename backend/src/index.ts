082f47ba12f19af487531eba8e794e756c54e0ee
naterpotatoers <nate.garza@hotmail.com>
2021-05-04 13:35:29 -0700

    adds winston logger logic

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,13 +1,18 @@
-import express from 'express'
+import express, { Request, Response } from 'express'
 import cors from 'cors'
 
+import logger from './utils/logger'
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 app.use(cors());
 
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello world!');
+});
+
 app.listen(5000, () => {
-  console.log('Server started on https://localhost:5000')
+  logger.info('Server started on http://localhost:5000');
 })
