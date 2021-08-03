529f49e2fba27f9e103b438b97cfe64774fa7c08
Nate Garza <nate.garza@hotmail.com>
2021-08-02 18:30:31 -0700

    adds query example of dynamo iot data

--- frontend/src/pages/All.js
+++ frontend/src/pages/All.js
@@ -1,9 +1,13 @@
+import { MyComponent } from "../components/LoadPlants";
+
 const All = () => {
-    return (
-      <div className="all">
-        <h2>All Plants</h2>
-      </div>
-    );
-  };
-  
-  export default All;
+  return (
+    <div className="all">
+      <h2>All Plants</h2>
+      <h3>Need to place chart here</h3>
+      <MyComponent />
+    </div>
+  );
+};
+
+export default All;
