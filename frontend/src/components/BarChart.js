371d6f54601fb016a5de25989e52934a3749acf3
roneil <roneilsembrano@gmail.com>
2021-08-02 19:36:33 -0700

    added bar chart

--- frontend/src/components/BarChart.js
+++ frontend/src/components/BarChart.js
@@ -1,26 +1,26 @@
-import "chart.js";
 import { Bar } from "react-chartjs-2";
 import React from "react";
 
-const Analitics = () => {
-    return (
-      <div>
-        Hello world
-       <Bar
-       data={{title: {text:'Daily Temperature'},
+
+
+export default class bChart extends React.Component {
+  render() {
+    return(
+    <div><Bar>
+      data={{
         labels: ['mon', 'tue', 'wed', 'thur', 'fri', 'sat', 'sun'],
         datasets:[{
           data:[78,79,85,98,90,98,97]
         }]
       }}
-      >
-       </Bar>
-
-      </div>
+      </Bar></div>   
 
 
+        
 
     );
-  };
+  }
   
-  export default Analitics;
+}
+
+
