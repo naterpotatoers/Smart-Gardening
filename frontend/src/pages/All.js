66962a16000e30249e315f02e95a89a15924aee0
Nate Garza <nate.garza@hotmail.com>
2021-09-09 16:48:17 -0700

    adds example query to frontend, grabbing iot data from /dyanmo endpoint

--- frontend/src/pages/All.js
+++ frontend/src/pages/All.js
@@ -1,9 +1,13 @@
+import { LoadPlants } from "../components/LoadPlants";
+
 const All = () => {
-    return (
-      <div className="all">
-        <h2>All Plants</h2>
-      </div>
-    );
-  };
-  
-  export default All;
+  return (
+    <div className="all">
+      <h2>All Plants</h2>
+      <h3>Need to place chart here</h3>
+      <LoadPlants />
+    </div>
+  );
+};
+
+export default All;
